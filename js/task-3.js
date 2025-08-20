dc53712404054d187465dd9b959bc1c4e6954657
Valerii <valeriidushyn@gmail.com>
2025-08-20 22:12:08 +0200

    hw-js-4

--- js/task-3.js
+++ js/task-3.js
@@ -13,11 +13,11 @@ class StringBuilder {
   }
 
   padStart(str) {
-    this.#value += str + this.#value;
+    this.#value = str + this.#value;
   }
 
   padBoth(str) {
-    this.#value += str + this.#value + str;
+    this.#value = str + this.#value + str;
   }
 }
 
